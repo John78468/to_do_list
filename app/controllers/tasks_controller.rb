737ade2543f87d65f1a730c3abe5d25b1b6236b0
cardon <philantrope1@outlook.com>
2024-09-11 17:09:46 +0200

    time is now

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,36 @@
 class TasksController < ApplicationController
+
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = task.new(task_params)
+    if t@task.save
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :completed)
+  end
 end
